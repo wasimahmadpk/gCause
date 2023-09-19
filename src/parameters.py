85721cebc5a7dec97c433ae21b88e80098d0874b
Wasim <wasimahmad.qc@gmail.com>
2023-09-19 19:07:22 +0200

    Params updated

--- src/parameters.py
+++ src/parameters.py
@@ -144,8 +144,8 @@ def get_climate_params():
     params = {
 
         'group_num': 2,    
-        'groups': {'g1': [0, 2], 'g2': [2, 4]},
-        'groups_size': {'g1':[2], 'g2':[2]},
+        'groups': {'g1': [0, 3], 'g2': [3, 6]},
+        'groups_size': {'g1':[3], 'g2':[3]},
         'epochs': 60,
         'pred_len': 20,
         'train_len': 555,
@@ -156,7 +156,7 @@ def get_climate_params():
         'win_size': 1,
         'slidingwin_size': 100,
         'step_size': 10,
-        'dim': 4,
+        'dim': 6,
         'batch_size': 32,
         'prior_graph': np.array([[1, 1, 1, 0, 1],
                                  [0, 1, 0, 0, 0],
@@ -180,19 +180,19 @@ def get_nino_params():
     params = {
 
         'group_num': 2,    
-        'groups': {'g1': [0, 5], 'g2': [5, 10]},    # 'g1': [0, 33], 'g2': [33, 69]
-        'groups_size': {'g1':[5], 'g2':[5]},
+        'groups': {'g1': [0, 2], 'g2': [2, 4]},    # 'g1': [0, 33], 'g2': [33, 69]
+        'groups_size': {'g1':[2], 'g2':[2]},
         'epochs': 75,
         'pred_len': 10,
-        'train_len': 150,
-        'num_layers': 6,
-        'num_cells': 60,
+        'train_len': 250,
+        'num_layers': 4,
+        'num_cells': 40,
         'num_samples': 10,
         'dropout_rate': 0.1,
         'win_size': 1,
         'slidingwin_size': 100,
         'step_size': 10,
-        'dim': 10,
+        'dim': 4,
         'batch_size': 32,
         'prior_graph': np.array([[1, 1, 1, 0, 1],
                                  [0, 1, 0, 0, 0],
