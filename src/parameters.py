3e8ee5050ff3ed5e9037757e5ce52a28ed15634f
Wasim <wasimahmad.qc@gmail.com>
2024-11-15 10:33:16 +0100

    params updated

--- src/parameters.py
+++ src/parameters.py
@@ -292,15 +292,15 @@ def get_rivernet_params_gc():
         'groups_cc': {'g1': [0, 1], 'g2': [1, 2], 'g3': [2, 3]}, #  
         'groups_size_cc': {'g1':[1], 'g2':[1], 'g3':[1]}, #    
 
-        'epochs': 500,
-        'pred_len': 5,
-        'train_len': 555,
+        'epochs': 55,
+        'pred_len': 10,
+        'train_len': 333,
         'num_layers': 4,
         'num_cells': 40,
         'num_samples': 2,
-        'dropout_rate': 0.10,
+        'dropout_rate': 0.5,
         'win_size': 1,
-        'num_sliding_win': 15, #21, 25
+        'num_sliding_win': 21, #21, 25
         'step_size': 10,
         'dim': 8,
         'dim_fs': 8,
