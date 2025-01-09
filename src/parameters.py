d8b075ec12b0f84723803ece315241b6f538d4ad
Wasim <wasimahmad.qc@gmail.com>
2025-01-09 14:00:50 +0100

    updates on syn experiments

--- src/parameters.py
+++ src/parameters.py
@@ -100,7 +100,7 @@ def get_syn_params():
         'num_samples': 5,
         'dropout_rate': 0.10,
         'win_size': 1,
-        'num_sliding_win': 15, #21, 25
+        'num_sliding_win': 21, #21, 25
         'step_size': 10,
         'dim': 8,
         'dim_fs': 8,
