072fd9f8ff797036b661c3cfd7746e2e566715f3
Wasim <wasimahmad.qc@gmail.com>
2025-01-10 16:11:41 +0100

    udpates

--- src/parameters.py
+++ src/parameters.py
@@ -92,16 +92,16 @@ def get_syn_params():
         'groups_cc': {'g1': [0, 1], 'g2': [1, 2], 'g3': [2, 3], 'g4': [3, 4]},      #  
         'groups_size_cc': {'g1':[1], 'g2':[1], 'g3':[1], 'g4':[1]},                 #    
         
-        'epochs': 33,
+        'epochs': 55,
         'pred_len': 10,
         'train_len': 666,
-        'num_layers': 5,
-        'num_cells': 40,
+        'num_layers': 6,
+        'num_cells': 66,
         'num_samples': 5,
         'dropout_rate': 0.10,
         'win_size': 1,
-        'num_sliding_win': 21, #21
-        'step_size': 10,
+        'num_sliding_win': 18,
+        'step_size': 3,
         'dim': 8,
         'dim_fs': 8,
         'dim_cc': 4,
