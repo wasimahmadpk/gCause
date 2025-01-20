7590e13d7e2aee63fc7af41e2924cfa69921a780
Wasim <wasimahmad.qc@gmail.com>
2025-01-20 17:59:31 +0100

    updates synX

--- src/parameters.py
+++ src/parameters.py
@@ -100,7 +100,7 @@ def get_syn_params():
         'num_samples': 5,
         'dropout_rate': 0.10,
         'win_size': 1,
-        'num_sliding_win': 50,
+        'num_sliding_win': 20,
         'step_size': 3,
         'dim': 8,
         'dim_fs': 8,
