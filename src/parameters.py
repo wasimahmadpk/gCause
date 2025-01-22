46fdf22d5cc45ebc7734e0ff81c0003272211998
Wasim <wasimahmad.qc@gmail.com>
2025-01-22 10:18:22 +0100

    parameters updated

--- src/parameters.py
+++ src/parameters.py
@@ -101,7 +101,7 @@ def get_syn_params():
         'num_samples': 5,
         'dropout_rate': 0.10,
         'win_size': 1,
-        'num_sliding_win': 18,
+        'num_sliding_win': 21,
         'step_size': 3,
         'dim': 8,
         'dim_fs': 8,
